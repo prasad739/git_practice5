564ddb1864b29af5570f297e773f74a154c92e1a
PRASAD <potnuru-laxmi.prasad@capgemini.com>
2023-08-17 22:18:17 +0530

    C++ added

---
+++ Demo2.java
@@ -0,0 +1,10 @@
+public class Demo{
+     public static void main(String[] args){
+	System.out.println("Java");
+	System.out.println("Python");
+	     System.out.println("C");
+ System.out.println("C++");
+
+	}
+
+ }
